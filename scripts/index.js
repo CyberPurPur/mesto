fafb33924c0b81f185f9bcff0cd12e1ec5c77a03
Nadezhda Orlova <n-orlova@yandex.ru>
2022-10-30 18:28:50 +0300

    Bugfixed after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,31 @@
-const PopupElement = document.querySelector(".popup");
-const PopupCloseButtonElement = PopupElement.querySelector(".popup__close-button");
-const ProfileEditButtonElement = document.querySelector(".profile__edit-button");
-const PopupFormFields = PopupElement.querySelector(".form__fields");
+const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
+const profileEditButtonElement = document.querySelector(".profile__edit-button");
+const popupFormFields = popupElement.querySelector(".form__fields");
 
-const nameInput = document.querySelector(".form__input_name");
-const workInput = document.querySelector(".form__input_work");
+const nameInput = document.querySelector(".form__fields_input_name");
+const workInput = document.querySelector(".form__fields_input_work");
 
 const nameElement = document.querySelector(".profile__name");
 const workElement = document.querySelector(".profile__description");
 
-const SubmitButton = document.querySelector(".form__submit-button");
-const FormElement = document.querySelector(".form");
+const submitButton = document.querySelector(".form__submit-button");
+const formElement = document.querySelector(".form");
 
-const openPopup = function () {
-  PopupElement.classList.add("popup__is-opened");
-  nameInput.value = nameElement.textContent;
-  workInput.value = workElement.textContent;
-};
-
-const closePopup = function () {
-  PopupElement.classList.remove("popup__is-opened");
-  FormElement.reset;
-};
-
-const ClosePopupByClickOnOverlay = function () {
-  if (event.target === event.currentTarget) {
-    closePopup();
-  }
-};
-
-function formSubmitHandler(evt) {
-  nameElement.textContent = nameInput.value;
-  workElement.textContent = workInput.value;
-  evt.preventDefault();
-  PopupElement.classList.remove("popup__is-opened");
+const actionPopup = function() {
+    popupElement.classList.toggle("popup_opened");
+    nameInput.value = nameElement.textContent;
+    workInput.value = workElement.textContent;
 }
 
-ProfileEditButtonElement.addEventListener("click", openPopup);
-PopupCloseButtonElement.addEventListener("click", closePopup);
-PopupElement.addEventListener("click", ClosePopupByClickOnOverlay);
-FormElement.addEventListener("submit", formSubmitHandler);
+formElement.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    nameElement.textContent = nameInput.value;
+    workElement.textContent = workInput.value;
+    popupElement.classList.remove("popup_opened");
+    });
+
+
+profileEditButtonElement.addEventListener("click", actionPopup);
+popupCloseButtonElement.addEventListener("click", actionPopup);
+
