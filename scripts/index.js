13a7ba8f623b27329a9590d549eb5b792a61ee07
Nadezhda Orlova <n-orlova@yandex.ru>
2022-10-31 12:17:28 +0300

    Bugfixed

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ const popupCloseButtonElement = popupElement.querySelector(".popup__close-button
 const profileEditButtonElement = document.querySelector(".profile__edit-button");
 const popupFormFields = popupElement.querySelector(".form__fields");
 
-const nameInput = document.querySelector(".form__input_name");
-const workInput = document.querySelector(".form__input_work");
+const nameInput = document.querySelector(".form__input_type_name");
+const workInput = document.querySelector(".form__input_type_work");
 
 const nameElement = document.querySelector(".profile__name");
 const workElement = document.querySelector(".profile__description");
@@ -13,19 +13,24 @@ const submitButton = document.querySelector(".form__submit-button");
 const formElement = document.querySelector(".form");
 
 const actionPopup = function() {
-    popupElement.classList.toggle("popup_opened");
-    nameInput.value = nameElement.textContent;
-    workInput.value = workElement.textContent;
+     popupElement.classList.toggle("popup_opened");
+    if
+    (popupElement.classList.contains('popup_opened'))
+    { nameInput.value = nameElement.textContent;
+      workInput.value = workElement.textContent;
+    }
 }
 
-formElement.addEventListener("submit", function(evt) {
+const formSubmitHandler = function (evt) {
     evt.preventDefault();
     nameElement.textContent = nameInput.value;
     workElement.textContent = workInput.value;
-    popupElement.classList.remove("popup_opened");
-    });
+    actionPopup();
+}
 
 
+formElement.addEventListener("submit", formSubmitHandler)
+    
 profileEditButtonElement.addEventListener("click", actionPopup);
 popupCloseButtonElement.addEventListener("click", actionPopup);
 
