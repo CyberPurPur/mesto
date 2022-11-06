8dbfd9c3645ba6c225af9690ae3d916246a882c0
Nadezhda Orlova <n-orlova@yandex.ru>
2022-11-06 22:35:24 +0300

    open ande close modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const popupElement = document.querySelector(".popup");
+const popupElement = document.querySelector(".popup_type_edit");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
 const profileEditButtonElement = document.querySelector(".profile__edit-button");
 const popupFormFields = popupElement.querySelector(".form__fields");
@@ -12,15 +12,34 @@ const workElement = document.querySelector(".profile__description");
 const submitButton = document.querySelector(".form__submit-button");
 const formElement = document.querySelector(".form");
 
+//для добавления картинок
+
+const placeAddBtn = document.querySelector('.profile__add-button');
+const newCardElement = document.querySelector(".popup_type_new-card");
+
+const placeNameInput = document.querySelector(".form__input_type_placename");
+const placeLinkInput = document.querySelector(".form__input_type_placelink");
+
+const placenameElement = document.querySelector('.element__description');
+const placeLinkElement = document.querySelector('.element__image');
+const cardCloseBtn = document.querySelector('#new-card-close');
+
+//открываем и закрываем редатирование карточки
+const newCardPopup = () =>
+newCardElement.classList.toggle("popup_opened");
+
+
+//открываем и закрываем редатирование профиля
 const actionPopup = function() {
-     popupElement.classList.toggle("popup_opened");
-    if
-    (popupElement.classList.contains('popup_opened'))
-    { nameInput.value = nameElement.textContent;
-      workInput.value = workElement.textContent;
-    }
+  popupElement.classList.toggle('popup_opened');
+ if
+ (popupElement.classList.contains('popup_opened'))
+ { nameInput.value = nameElement.textContent;
+   workInput.value = workElement.textContent;
+ }
 }
 
+//добавлякм данные в профиль
 const formSubmitHandler = function (evt) {
     evt.preventDefault();
     nameElement.textContent = nameInput.value;
@@ -28,9 +47,14 @@ const formSubmitHandler = function (evt) {
     actionPopup();
 }
 
+//слушатели
+formElement.addEventListener("submit", formSubmitHandler);
 
-formElement.addEventListener("submit", formSubmitHandler)
-    
 profileEditButtonElement.addEventListener("click", actionPopup);
+
+
 popupCloseButtonElement.addEventListener("click", actionPopup);
+
+placeAddBtn.addEventListener('click', newCardPopup);
+cardCloseBtn.addEventListener('click', newCardPopup);
 
