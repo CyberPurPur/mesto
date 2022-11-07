68de795dce699cda7058996e1c2edc7354b4bffe
Nadezhda Orlova <n-orlova@yandex.ru>
2022-11-07 14:42:37 +0300

    added trash icon

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ const actionPopup = function() {
  }
 }
 
-//добавлякм данные в профиль
+//добавляем данные в профиль
 const formSubmitHandler = function (evt) {
     evt.preventDefault();
     nameElement.textContent = nameInput.value;
@@ -52,9 +52,39 @@ formElement.addEventListener("submit", formSubmitHandler);
 
 profileEditButtonElement.addEventListener("click", actionPopup);
 
-
 popupCloseButtonElement.addEventListener("click", actionPopup);
 
 placeAddBtn.addEventListener('click', newCardPopup);
 cardCloseBtn.addEventListener('click', newCardPopup);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('.img_template');
+const container = document.querySelector('.elements');
 
